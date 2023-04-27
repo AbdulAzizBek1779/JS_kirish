6081d4854ca80b474ff4ad2c8fa79a9848fbb57b
AbdulAzizBek1779 <131785350+AbdulAzizBek1779@users.noreply.github.com>
2023-04-27 09:52:53 +0500

    Add files via upload

---
+++ JS Kirish/index.js
@@ -0,0 +1,162 @@
+// boolean  mantiqiy ifoda true false 1  0   
+
+
+// console.log(false);
+// console.log(true);
+
+// let a = 1;
+
+//  c = a + 1;  // 1 + 1 = 2;
+
+// console.log(c);
+
+
+// let a = 2, b = 3,c = 5;
+// summ = a + b + c;
+
+// console.log(summ);
+
+// let x = 1,
+// y = 2;
+
+// let a = b = c = 3;
+
+// console.log(a,b,c);
+
+// "" '' ``
+
+// let text ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aperiam quibusdam, adipisci soluta quasi est tempora repellat ipsam? Voluptatibus, cupiditate? Accusamus alias accusantium sed id ducimus exercitationem vel eos illum?";
+
+// console.log(text);
+
+// let brand = "United IT Academy";
+//  let name =  "IT center";
+
+// console.log(brand,name);
+
+// let name = "UITC";
+
+// console.log("Welcome to");
+
+// incriment va decriment
+
+// let a = 7,b = 2;
+
+// a++;
+
+// a--;
+
+// a = a + 1;
+// a++;
+// a = a - b;   //  7 - 2 = 5  a = 5
+// a = a + b; // 5 + 2 = 7
+
+// a = a / b;
+
+
+// c = a % b;  1 
+
+// c = a * b;
+
+// console.log(c);
+// malumot turi data type number,string
+// let a = 10;
+
+// let b = "10afd";
+
+// // c = a + b;
+
+// // NaN null
+// // console.log(typeof(a), typeof(b));
+// // console.log( a / b)  // 20 
+// console.log(a * b)
+
+
+// console.log(typeof(true));
+// console.log(typeof(false));
+
+
+
+// !,!=,==,>=,<=,>,< &&  
+
+// let m = false;
+
+// n = 0;
+// let m = true;
+
+// n = 1;
+
+// console.log(m,n);
+
+// console.log(true && true);
+// console.log(false && true);
+// console.log(true && false);
+// console.log(false && false);
+
+
+// console.log(true  true);
+// console.log(false  true);
+// console.log(true  false);
+// console.log(false || false);
+
+
+
+// let a = null;
+
+// let x;
+
+// console.log(null + 1);
+
+// console.log(undefined + 1);
+
+// console.log(typeof(null));
+// ================================
+// object  key 
+// let car = {
+//     name:"Cobalt",
+//     Price:200000,
+//     Color:"Black"
+// };
+
+// console.log(car.Color);
+// ----------------------------------
+// // array  index
+
+//  let   arr = ['Olma',12,"Apelsin", obj={name:"spark"}, 500, "Nuriddin"];  
+
+//  console.log(arr[10]);
+
+
+// let name1 = "Nuriddin";
+// let name2 = "saida";
+
+// console.log(name1 ,''+ name2);  
+
+// let a = Symbol(12);
+// console.log(a);
+
+
+// console.log(9007199254740992n + 2n);
+// methods  floor ceil round.......
+
+// let num  = 8.5;
+
+// console.log(Math.floor(8.5));
+// console.log(Math.ceil(8.5));
+// console.log(Math.round(8.4));
+
+// console.log(Math.cbrt(27));  // kub chiqarib beradi
+
+// console.log(Math.pow(4,2));
+// console.log(Math.pow(4,4));
+
+// console.log(Math.sqrt(16)); ildiz
+// console.log(Math.sqrt(81)); 
+
+// // min max 
+// console.log("Sonni eng kichigi = ", + Math.min(2,55,7,25,8) );
+// console.log("Sonni eng kattasi = ", + Math.max(2,55,7,25,8) );
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-5))
